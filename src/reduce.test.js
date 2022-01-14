67d0c34b3f97cb7a46f0b329024645fdd95a2cde
mgrinko <glukym@gmail.com>
2022-01-14 09:41:35 +0200

    fix linter

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -1,13 +1,19 @@
+'use strict';
+
 const { reduce } = require('./reduce');
 
 describe('reduce', () => {
   beforeAll(() => {
-    Array.prototype.reduce2 = reduce;
+    Array.prototype.reduce2 = reduce; // eslint-disable-line
   });
 
   afterAll(() => {
     delete Array.prototype.reduce2;
-  })
+  });
+
+  it('should ', () => {
+
+  });
 
   // Add tests here
 });
