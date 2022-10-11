26063ffc58cc3e72d8cb4be6191734a75d9eeee6
Dmytro Semenov <Dmytro.Semenov@tx.local>
2022-10-11 23:56:06 +0300

    Solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -3,6 +3,13 @@
 const { reduce } = require('./reduce');
 
 describe('reduce', () => {
+  const data = [1, 2, 3, 4, 5];
+  let callback;
+
+  beforeEach(() => {
+    callback = jest.fn((prev, x) => prev + x);
+  });
+
   beforeAll(() => {
     Array.prototype.reduce2 = reduce; // eslint-disable-line
   });
@@ -11,9 +18,47 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(data.reduce2).toBeInstanceOf(Function);
   });
 
-  // Add tests here
+  it('callback should be called for each value in the array', () => {
+    data.reduce2(callback, 10);
+
+    expect(callback).toHaveBeenCalledTimes(data.length);
+  });
+
+  it('should return correct value', () => {
+    const result = data.reduce2(callback, 10);
+
+    ;
+
+    expect(result).toBe(25);
+  });
+
+  it('should be called every callbacks with the correct parameters', () => {
+    data.reduce2(callback, 1);
+
+    expect(callback).toHaveBeenCalledWith(1, 1, 0, data);
+    expect(callback).toHaveBeenCalledWith(2, 2, 1, data);
+    expect(callback).toHaveBeenNthCalledWith(5, 11, 5, 4, data);
+  });
+
+  it('A callback should not be called for an empty array', () => {
+    [].reduce2(callback, 1);
+
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  it(`should return the data type, which must`
+    + ` be the same as startValue`, () => {
+    const startValue = 1;
+    const result = data.reduce2(callback, startValue);
+
+    const startValue2 = '';
+    const result2 = ['H', 'e', 'l', 'l', 'o'].reduce2(callback, startValue2);
+
+    expect(typeof result).toBe(typeof startValue);
+    expect(typeof result2).toBe(typeof startValue2);
+  });
 });
