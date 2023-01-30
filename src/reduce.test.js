103183c63bec677727327babfb2482f340e9bb8c
Usenko Bohdana <usenkobogdana19@gmail.com>
2023-01-30 22:19:34 +0200

    solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,35 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
+  it('should run callback the correct number of times', () => {
+    const callback = jest.fn();
 
+    [1, 2, 3, 4, 5].reduce2(callback, 5);
+
+    expect(callback).toHaveBeenCalledTimes(5);
   });
 
-  // Add tests here
+  it('should return `startValue` if array is empty', () => {
+    const sum = [].reduce2((a, b) => a + b, 4);
+
+    expect(sum).toBe(4);
+  });
+
+  it('should run callback with the correct parameters', () => {
+    const callback = jest.fn((a, b) => a + b);
+
+    [1, 2, 3, 4, 5].reduce2(callback, 5);
+
+    expect(callback.mock.calls[0]).toEqual([5, 1, 0, [1, 2, 3, 4, 5]]);
+    expect(callback.mock.calls[1]).toEqual([6, 2, 1, [1, 2, 3, 4, 5]]);
+    expect(callback.mock.calls[2]).toEqual([8, 3, 2, [1, 2, 3, 4, 5]]);
+    expect(callback.mock.calls[3]).toEqual([11, 4, 3, [1, 2, 3, 4, 5]]);
+    expect(callback.mock.calls[4]).toEqual([15, 5, 4, [1, 2, 3, 4, 5]]);
+  });
+
+  it('should return the correct value', () => {
+    const sum = [1, 2, 3, 4, 5].reduce2((a, b) => a + b, 3);// start value - 10
+
+    expect(sum).toBe(18);
+  });
 });
