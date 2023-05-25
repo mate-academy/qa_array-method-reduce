56e6787eff7d8705256c28d052a4c4d006e35dc3
Dima Cherednyk <dicherednyk@gmail.com>
2023-05-25 21:47:13 +0300

    Solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,39 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(reduce).toBeInstanceOf(Function);
   });
 
-  // Add tests here
+  it('should not call a callback for an empty array', () => {
+    const f = jest.fn();
+
+    expect(f).not.toHaveBeenCalled();
+  });
+
+  it('should call a callback once per item', () => {
+    const f = jest.fn();
+
+    [1, 2, 3].reduce(f, 0);
+
+    expect(f).toHaveBeenCalledTimes(3);
+  });
+
+  it('should work without startValue', () => {
+    const f
+      = jest.fn((accumulator, currentValue) => accumulator + currentValue);
+
+    expect([1, 2, 3].reduce(f)).toBe(6);
+  });
+
+  it('should pass previous element, current element, index, array', () => {
+    const f
+      = jest.fn((accumulator, currentValue) => accumulator + currentValue);
+
+    [1, 2, 3].reduce(f, 0);
+
+    expect(f).toHaveBeenCalledWith(0, 1, 0, [1, 2, 3]);
+    expect(f).toHaveBeenNthCalledWith(2, 1, 2, 1, [1, 2, 3]);
+    expect(f.mock.calls[2]).toEqual([3, 3, 2, [1, 2, 3]]);
+  });
 });
