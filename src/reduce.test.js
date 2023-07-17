e0375ed5a7bb0d6e08bf16c16c6d17765e21042f
Dmytro Budynskyi <dbudinskyy@gmail.com>
2023-07-17 16:58:44 +0200

    Solution 17072023

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,35 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
+  // Add tests here
 
+  it('should return initialValue for an empty array', () => {
+    const f = jest.fn();
+
+    const result = [].reduce2(f, 5);
+
+    expect(result).toBe(5);
   });
 
-  // Add tests here
+  it('should pass value from the previous call,'
+  + 'an element, an index and an array to a callback', () => {
+    const items = [10, 20, 30, 40];
+    const f = jest.fn((prev, current) => prev + current);
+
+    items.reduce2(f, 0);
+
+    expect(f).toHaveBeenNthCalledWith(1, 0, 10, 0, items);
+    expect(f).toHaveBeenNthCalledWith(2, 10, 20, 1, items);
+    expect(f).toHaveBeenNthCalledWith(3, 30, 30, 2, items);
+    expect(f).toHaveBeenNthCalledWith(4, 60, 40, 3, items);
+  });
+
+  it('should return correct value', () => {
+    const items = [10, 20, 30, 40];
+    const f = jest.fn((prev, current) => prev + current);
+
+    const result = items.reduce2(f, 0);
+
+    expect(result).toBe(100);
+  });
 });
