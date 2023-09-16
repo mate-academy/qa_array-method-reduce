493e62a67026488a3542be74ba671681f3a12999
Bohdan Svystun <bogdansvystun02@gmail.com>
2023-09-16 14:41:36 +0300

    Solution [Bohdan Svystun]

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,47 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
+  it('should reduce an array to a single value by applying a callback', () => {
+    const arr = [1, 2, 3, 4, 5];
 
+    const sum = arr.reduce2((acc, curr) => acc + curr, 0);
+
+    expect(sum).toBe(15); // 1 + 2 + 3 + 4 + 5 = 15
   });
 
-  // Add tests here
+  it('should reduce an array  "[1, 2, 3, 4, 5]"'
+   + 'to a single value by applying a callback', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const product = arr.reduce2((acc, curr) => acc * curr, 1);
+
+    expect(product).toBe(120); // 1 * 2 * 3 * 4 * 5 = 120
+  });
+
+  it('should work with an empty array and an initial value', () => {
+    const arr = [];
+
+    const result = arr.reduce2((acc, curr) => acc + curr, 10);
+
+    expect(result).toBe(10);
+  });
+
+  it('should work with an empty array and an initial value', () => {
+    const arr = [];
+
+    const product = arr.reduce2((acc, curr) => acc * curr, 5);
+
+    expect(product).toBe(5);
+  });
+
+  it('should handle different data types', () => {
+    const arr = ['b', 'i', 'g'];
+
+    const concat = arr.reduce2((acc, curr) => acc + curr, '');
+
+    expect(concat).toBe('big');
+
+    const isTrue = arr.reduce2((acc, curr) => acc && curr, true);
+
+    expect(isTrue).toBeTruthy();
+  });
 });
