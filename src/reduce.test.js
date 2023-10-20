795ca51ccf88408f622b488486c93606fb5761e9
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-10-20 19:09:19 +0200

    reduce

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -3,35 +3,71 @@
 const { reduce } = require("./reduce");
 
 describe("reduce", () => {
-  it("should return startValue if array is empty", () => {
-    const sum = reduce([], (a, b) => a + b, 9);
-
-    expect(sum).toBe(9);
+  beforeAll(() => {
+    Array.prototype.reduce2 = reduce; // eslint-disable-line
   });
 
-  it("should run callback the correct number of times", () => {
-    const callback = jest.fn();
-
-    reduce([1, 2, 3, 4, 5], callback, 9);
-
-    expect(callback).toHaveBeenCalledTimes(5);
+  afterAll(() => {
+    delete Array.prototype.reduce2;
   });
 
-  it("should run callback with the correct parameters", () => {
-    const callback = jest.fn((a, b) => a + b);
+  it("should reduce an array to a single value", () => {
+    const array = [1, 2, 3, 4, 5];
+    const sum = array.reduce2((acc, curr) => acc + curr);
 
-    reduce([1, 2, 3, 4, 5], callback, 9);
-
-    expect(callback.mock.calls[0]).toEqual([9, 1, 0, [1, 2, 3, 4, 5]]);
-    expect(callback.mock.calls[1]).toEqual([10, 2, 1, [1, 2, 3, 4, 5]]);
-    expect(callback.mock.calls[2]).toEqual([12, 3, 2, [1, 2, 3, 4, 5]]);
-    expect(callback.mock.calls[3]).toEqual([15, 4, 3, [1, 2, 3, 4, 5]]);
-    expect(callback.mock.calls[4]).toEqual([19, 5, 4, [1, 2, 3, 4, 5]]);
+    expect(sum).toBe(15);
   });
 
-  it("should return the correct value", () => {
-    const sum = reduce([1, 2, 3, 4, 5], (a, b) => a + b, 10);
+  it("should reduce an array starting from a specified initial value", () => {
+    const array = [1, 2, 3, 4, 5];
+    const product = array.reduce2((acc, curr) => acc * curr, 1);
 
-    expect(sum).toBe(25);
+    expect(product).toBe(120);
+  });
+
+  it("should work with an empty array and no initial value", () => {
+    const array = [];
+    const result = array.reduce2((acc, curr) => acc + curr);
+
+    expect(result).toBeUndefined();
+  });
+
+  it("should throw an error if the callback function is not provided", () => {
+    const array = [1, 2, 3, 4, 5];
+
+    expect(() => array.reduce2()).toThrowError(TypeError);
+  });
+
+  it("should iterate over array elements in the correct order", () => {
+    const array = ["a", "b", "c", "d"];
+    const result = array.reduce2((acc, curr) => acc + curr);
+
+    expect(result).toBe("abcd");
+  });
+
+  it("should modify the startValue if the callback function modifies it", () => {
+    const array = [1, 2, 3, 4, 5];
+    const startValue = { value: 0 };
+
+    array.reduce2((acc, curr) => {
+      acc.value += curr;
+
+      return acc;
+    }, startValue);
+    expect(startValue.value).toBe(15);
+  });
+
+  it("should return the single element of the array if it has only one element", () => {
+    const array = [42];
+    const result = array.reduce2((acc, curr) => acc + curr);
+
+    expect(result).toBe(42);
+  });
+
+  it("should reduce an array of objects to a single value", () => {
+    const array = [{ value: 1 }, { value: 2 }, { value: 3 }];
+    const sum = array.reduce2((acc, curr) => acc + curr.value, 0);
+
+    expect(sum).toBe(6);
   });
 });
