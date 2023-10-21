4de4f9b4129392a34448567fb40cbd085c585d8d
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-10-21 21:29:03 +0200

    correct solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -3,70 +3,54 @@
 const { reduce } = require("./reduce");
 
 describe("reduce", () => {
-  it("should be declared and be an instance of Function", () => {
-    expect(reduce).toBeInstanceOf(Function);
+  beforeAll(() => {
+    Array.prototype.reduce2 = reduce;
   });
 
-  it("should call a callback once per item if initialValue specified", () => {
-    const items = [1, 2, 3, 4, 5];
-    const f = jest.fn();
-
-    reduce(items, f, 0);
-
-    expect(f).toHaveBeenCalledTimes(5);
+  afterAll(() => {
+    delete Array.prototype.reduce2;
   });
 
-  it(
-    "should call a callback (array.length - 1) times" +
-      " if initialValue did not specified",
-    () => {
-      const items = [1, 2, 3, 4, 5];
-      const f = jest.fn();
+  it("should handle cases with an initial value", () => {
+    const arr = [1, 2, 3, 4, 5];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const initialValue = 10;
 
-      reduce(items, f);
+    const result = arr.reduce2(reducer, initialValue);
+    const expected = arr.reduce(reducer, initialValue);
 
-      expect(f).toHaveBeenCalledTimes(4);
-    }
-  );
-
-  it("should not call a callback for an empty array", () => {
-    const f = jest.fn();
-
-    reduce([], f);
-
-    expect(f).not.toHaveBeenCalled();
+    expect(result).toBe(expected);
   });
 
-  it("should return initialValue for an empty array", () => {
-    const f = jest.fn();
+  it("should work with an empty array", () => {
+    const arr = [];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const initialValue = 0;
 
-    const result = reduce([], f, 5);
+    const result = arr.reduce2(reducer, initialValue);
+    const expected = arr.reduce(reducer, initialValue);
 
-    expect(result).toBe(5);
+    expect(result).toBe(expected);
   });
 
-  it(
-    "should pass value from the previous call," +
-      " an element, an index and an array to a callback",
-    () => {
-      const items = [10, 20, 30, 40];
-      const f = jest.fn((prev, current) => prev + current);
+  it("should work without an initial value", () => {
+    const arr = [1, 2, 3, 4, 5];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-      reduce(items, f, 0);
+    const result = arr.reduce2(reducer);
+    const expected = arr.reduce(reducer);
 
-      expect(f).toHaveBeenNthCalledWith(1, 0, 10, 0, items);
-      expect(f).toHaveBeenNthCalledWith(2, 10, 20, 1, items);
-      expect(f).toHaveBeenNthCalledWith(3, 30, 30, 2, items);
-      expect(f).toHaveBeenNthCalledWith(4, 60, 40, 3, items);
-    }
-  );
+    expect(result).toBe(expected);
+  });
 
-  it("should return the correct value", () => {
-    const items = [10, 20, 30, 40];
-    const f = jest.fn((prev, current) => prev + current);
+  it("should handle array contains undefined or empty elements", () => {
+    const arr = [1, undefined, 2, "", 3, null, 4, false, 5];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const initialValue = 0;
 
-    const result = reduce(items, f, 0);
+    const result = arr.reduce2(reducer, initialValue);
+    const expected = arr.reduce(reducer, initialValue);
 
-    expect(result).toBe(100);
+    expect(result).toBe(expected);
   });
 });
