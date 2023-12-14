7aaf9a66c6a1adfe40b540592125da9d4ec4ee3f
sheva-laptop <vladislavshevchenko10@gmail.com>
2023-12-14 19:28:28 +0200

    add tests

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,22 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
+  it('should reduce an array without an initial value', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const callback = jest.fn((acc, current) => acc + current);
+    const result = arr.reduce2(callback);
 
+    expect(callback).toHaveBeenCalledTimes(4);
+    expect(result).toBe(15);
   });
 
-  // Add tests here
+  it('should reduce an array with an initial value', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const callback = jest.fn((acc, current) => acc + current);
+    const initialValue = 10;
+    const result = arr.reduce2(callback, initialValue);
+
+    expect(callback).toHaveBeenCalledTimes(5);
+    expect(result).toBe(25);
+  });
 });
