490017b8cd7c4c6b198fd11d71a1adaab6581af0
Eugene Khablenko <eugene.khablenko@gmail.com>
2023-12-20 03:06:07 +0200

    solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,78 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
-
+  it('declared', () => {
+    expect(Array.prototype.reduce2).toBeInstanceOf(Function);
   });
 
-  // Add tests here
+  it('simple array and accumulator', () => {
+    const array = [1, 2, 3, 4, 5];
+    const result = array.reduce2((acc, el) => {
+      return acc + el;
+    }, 0);
+
+    expect(result).toBe(15);
+  });
+
+  it('empty array', () => {
+    const array = [];
+    const result = array.reduce2((acc, el) => {
+      return acc + el;
+    }, 0);
+
+    expect(result).toBe(0);
+  });
+
+  it('works with an initialValue > 0 correctly', () => {
+    const array = [1, 2, 3];
+
+    const result = array.reduce2((acc, el) => {
+      return acc + el;
+    }, 10);
+
+    expect(result).toBe(16);
+  });
+
+  it('works correctly with an object accumulator', () => {
+    const array = [1, 2, 3, 4, 5];
+
+    const result = array.reduce2(
+      (acc, el) => {
+        return { sum: acc.sum + el };
+      },
+      { sum: 0 }
+    );
+
+    expect(result).toStrictEqual({ sum: 15 });
+  });
+
+  it('concatenates strings', () => {
+    const array = ['d', 'e', 'f', 'g'];
+
+    const result = array.reduce2((acc, el) => {
+      return acc + el;
+    }, 'abc');
+
+    expect(result).toBe('abcdefg');
+  });
+
+  it("doesn't mutate original array", () => {
+    const array = [1, 2, 3, 4, 5];
+
+    array.reduce2((acc, el) => {
+      return acc + el;
+    }, 0);
+
+    expect(array).toStrictEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('should reduce w/o start value', () => {
+    const array = [1, 2, 3, 4, 5];
+
+    const result = array.reduce2((acc, el) => {
+      return acc + el;
+    });
+
+    expect(result).toBe(15);
+  });
 });
