a4afdd5ee5fd94a52a15caae03bf8e59b835faa1
Kate <ekaterina2002aleks@gmail.com>
2023-12-29 19:00:05 +0200

    Solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,24 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
+  it('should right work with function max', () => {
+    const expected = 100;
+    const f = (a, b) => Math.max(a, b);
 
+    expect([1, 100].reduce(f, 50)).toEqual(expected);
   });
 
-  // Add tests here
+  it('should right work with one element in arr', () => {
+    const expected = 50;
+    const f = (a, b) => Math.max(a, b);
+
+    expect([50].reduce(f, 10)).toEqual(expected);
+  });
+
+  it('should right work without elements in arr', () => {
+    const expected = 1;
+    const f = (a, b) => Math.max(a, b);
+
+    expect([].reduce(f, 1)).toEqual(expected);
+  });
 });
