bd185d93b3cca2887c399bf0d3bd68dc712e5a99
Kate <ekaterina2002aleks@gmail.com>
2024-01-10 20:45:06 +0200

    Solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -15,20 +15,41 @@ describe('reduce', () => {
     const expected = 100;
     const f = (a, b) => Math.max(a, b);
 
-    expect([1, 100].reduce(f, 50)).toEqual(expected);
+    expect([1, 100].reduce2(f, 50)).toEqual(expected);
   });
 
   it('should right work with one element in arr', () => {
     const expected = 50;
     const f = (a, b) => Math.max(a, b);
 
-    expect([50].reduce(f, 10)).toEqual(expected);
+    expect([50].reduce2(f, 10)).toEqual(expected);
   });
 
   it('should right work without elements in arr', () => {
     const expected = 1;
     const f = (a, b) => Math.max(a, b);
 
-    expect([].reduce(f, 1)).toEqual(expected);
+    expect([].reduce2(f, 1)).toEqual(expected);
+  });
+
+  it('should right work without elements in arr for min', () => {
+    const expected = 1;
+    const f = (a, b) => Math.min(a, b);
+
+    expect([].reduce2(f, 1)).toEqual(expected);
+  });
+
+  it('should right work with function min', () => {
+    const expected = 1;
+    const f = (a, b) => Math.min(a, b);
+
+    expect([1, 100].reduce2(f, 50)).toEqual(expected);
+  });
+
+  it('should right work with one element in arr', () => {
+    const expected = 50;
+    const f = (a, b) => Math.min(a, b);
+
+    expect([50].reduce2(f, 10)).toEqual(expected);
   });
 });
