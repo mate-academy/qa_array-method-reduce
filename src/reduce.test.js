04f3f592663e76d6b5e4de34dae1fef04c9b2df3
Dmytro <dakorozer@gmail.com>
2024-05-05 09:46:39 +0300

    solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,39 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(Array.prototype.reduce2).toBeInstanceOf(Function);
   });
 
-  // Add tests here
+  it('should work with an array of numbers', () => {
+    const array = [1, 2, 3, 4];
+
+    const result = array.reduce2((a, b) => a + b, 0);
+
+    expect(result).toBe(10);
+  });
+
+  it('should work with an array of strings', () => {
+    const array = ['q', 'w', 'e', 'r'];
+
+    const result = array.reduce2((a, b) => a + b, '');
+
+    expect(result).toBe('qwer');
+  });
+  
+  it('should work with an empty array', () => {
+    const array = [];
+
+    const result = array.reduce2((a, b) => a + b, 0);
+
+    expect(result).toBe(0);
+  });
+  
+  it('should work without an initial value', () => {
+    const array = [1, 2, 3];
+
+    const result = array.reduce2((a, b) => a + b);
+
+    expect(result).toBe(6);
+  });
 });
