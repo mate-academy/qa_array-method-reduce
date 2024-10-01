777e1f586c84a724d987aa19c86ff340afecddb0
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2024-10-01 16:15:34 +0200

    solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,58 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
-
+  it('should reduce be defined', () => {
+    expect(reduce).toBeDefined();
   });
 
-  // Add tests here
+  it('should add all values from numbers array', () => {
+    const numbers = [1, 2, 3, 4];
+    const result = numbers.reduce2(
+      (accumulator, currentVal) => accumulator + currentVal
+    );
+
+    expect(result).toEqual(10);
+  });
+
+  it('should multiplay all values from numbers array', () => {
+    const numbers = [1, 2, 3, 4];
+    const result = numbers.reduce2(
+      (accumulator, currentVal) => accumulator * currentVal
+    );
+
+    expect(result).toEqual(24);
+  });
+
+  it(`should add all values from numbers array,
+    increased by initial value`, () => {
+    const numbers = [1, 2, 3, 4];
+    const result = numbers.reduce2(
+      (accumulator, currentVal) => accumulator + currentVal,
+      3
+    );
+
+    expect(result).toEqual(13);
+  });
+
+  it(`should concatenate all strings from array to one message`, () => {
+    const strings = ['Hello', 'world', `it's`, 'me', 'Mario!'];
+    const result = strings.reduce2(
+      (accumulator, currentVal) => accumulator + ' ' + currentVal
+    );
+
+    expect(result).toEqual(`Hello world it's me Mario!`);
+  });
+
+  it('should return the initial value for an empty array', () => {
+    const result = [].reduce2(() => {}, 'Mario');
+
+    expect(result).toEqual('Mario');
+  });
+
+  it(`should return first element if there is only one in array,
+    and there is no initial value provided`, () => {
+    const result = [99].reduce2(() => {});
+
+    expect(result).toEqual(99);
+  });
 });
