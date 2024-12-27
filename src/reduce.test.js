ea85633bb57096c54afae75fe632a3dd4562087b
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-12-27 13:51:58 +0200

    the task is complete

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-const { reduce } = require('./reduce');
+const { reduce } = require("./reduce");
 
-describe('reduce', () => {
+describe("reduce", () => {
   beforeAll(() => {
     Array.prototype.reduce2 = reduce; // eslint-disable-line
   });
@@ -11,9 +11,42 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
-
+  it(`should be declared`, () => {
+    expect(Array.prototype.reduce2).toBeInstanceOf(Function);
   });
 
-  // Add tests here
+  it(`should be correctly if 'startValue' === 0`, () => {
+    const result = [1, 2, 3].reduce2((acc, num) => acc + num, 0);
+
+    expect(result).toBe(6);
+  });
+
+  it(`should be correctly if 'startValue' > 0`, () => {
+    const result = [1, 2, 3].reduce2((acc, num) => acc + num, 6);
+
+    expect(result).toBe(12);
+  });
+
+  it(`should be correctly if without 'startValue'`, () => {
+    const result = [1, 2, 3].reduce2((acc, num) => acc + num);
+
+    expect(result).toBe(6);
+  });
+
+  it(`should be correctly if string array`, () => {
+    const result = ["Dima", " ", "Boiko"].reduce2((acc, str) => acc + str);
+
+    expect(result).toBe("Dima Boiko");
+  });
+
+  it("should pass the correct parameters to the callback", () => {
+    const mockCallback = jest.fn((acc, num, index, array) => acc + num);
+
+    [1, 2, 3].reduce2(mockCallback, 0);
+
+    expect(mockCallback).toHaveBeenCalledTimes(3);
+    expect(mockCallback).toHaveBeenCalledWith(0, 1, 0, [1, 2, 3]);
+    expect(mockCallback).toHaveBeenCalledWith(1, 2, 1, [1, 2, 3]);
+    expect(mockCallback).toHaveBeenCalledWith(3, 3, 2, [1, 2, 3]);
+  });
 });
