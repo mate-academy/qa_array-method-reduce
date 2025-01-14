4c8e6a35a70b1dcc8e4f8c09e01d69f3d6bf9fe4
Vlad Kachmazov <kachevl@gmail.com>
2025-01-14 17:24:21 +0300

    solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -3,17 +3,51 @@
 const { reduce } = require('./reduce');
 
 describe('reduce', () => {
+  const testArr = [1, 10, 15, 20, 50, 100];
+
+  let cb;
+
   beforeAll(() => {
     Array.prototype.reduce2 = reduce; // eslint-disable-line
+  });
+
+  beforeEach(() => {
+    cb = jest.fn((...args) => {
+      return [...args][0] + [...args][1];
+    });
   });
 
   afterAll(() => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
+  it('should return value', () => {
+    const result = testArr.reduce2(cb, 0);
 
+    expect(result).not.toBeUndefined();
   });
 
-  // Add tests here
+  it('callback should be called array.length times', () => {
+    testArr.reduce2(cb, 0);
+
+    expect(cb.mock.calls.length).toBe(testArr.length);
+  });
+
+  it('callback should be called with four arguments', () => {
+    testArr.reduce2(cb, 0);
+
+    expect(cb.mock.calls[0].length).toBe(4);
+  });
+
+  it('returns start value for the empty array', () => {
+    const result = [].reduce2(cb, 1000);
+
+    expect(result).toBe(1000);
+  });
+
+  it('callback shouldn`t be called for the empty array', () => {
+    [].reduce2(cb, 1000);
+
+    expect(cb.mock.calls.length).toBeFalsy();
+  });
 });
